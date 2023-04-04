6a281fe1af64e24482511d2af77af75e2cefe764
ian7525 <ian7525@gmail.com>
2023-04-04 23:27:07 +0800

    add linebot lib

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 import express from "express"
 import * as dotenv from "dotenv"
 
+import linebotLib from "./lib/linebot.js"
+
 dotenv.config()
 const app = express()
+
+const linebot = linebotLib(process.env)
 
 app.get("/health", (req, res) => {
   res.status(200).send({ message: "OK" })
 })
+
+app.post("/callback", linebot.lineMw, (req, res) => {
+  console.log(req.body)
+  Promise.all(req.body.events.map(handler))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err)
+      res.status(500).end()
+    })
+})
+
+function handler(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null)
+  }
+
+  const result = { type: "text", text: event.message.text }
+
+  return linebot.lineClient.replyMessage(event.replyToken, result)
+}
 
 // listen on port
 const port = process.env.PORT || 5000
